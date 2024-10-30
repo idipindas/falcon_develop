e30d85b3e7b42b5c40d34c88bfef110ce3ebba6a
Dipindas A H <dipindas@expressare.com>
2024-10-30 13:36:58 +0530

    industrieso page

---
+++ src/app/industries/page.tsx
@@ -0,0 +1,13 @@
+
+import Industries from "@/pages/industries/Industries";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Industries />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/pages/industries/Industries.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PrimarySection from './components/PrimarySection';
+import SecondarySection from './components/SecondarySection';
+import IndustriesCard from './components/IndustriesCard';
+
+const Industries = () => {
+    return (
+        <div>
+            <PrimarySection/>
+            <SecondarySection/>
+            <IndustriesCard/>
+        </div>
+    );
+}
+
+export default Industries;
